48a68b50f625c29a00a96881f8575df7505587eb
sunagimo-syakunage <cfgy7u6@gmail.com>
2019-03-24 21:02:50 +0900

    TOPスライドショーのみほか微調整

--- js/script.js
+++ js/script.js
@@ -17,4 +17,17 @@ $(function() {
       $('.hamburger-btn').removeClass('hamburger-btn-not-on-header');
     }
   });
+  $('.top-main-slider-items').slick({
+  centerMode: true, //センターモード
+  centerPadding: '0', //前後のパディング
+  autoplay: true, //オートプレイ
+  autoplaySpeed: 5000, //オートプレイの切り替わり時間
+  slidesToShow: 1,
+  fade: true,
+  pauseOnHover: true,
+  dots: true,
+  arrows: true,
+  prevArrow: '<button type="button" class="top-slick-arrow-topics slick-prev-topics"><img src="./media/top/top_left_arrow.png"></button>',
+  nextArrow: '<button type="button" class="top-slick-arrow-topics slick-next-topics"><img src="./media/top/top_right_arrow.png"></button>',
+  });
 });
